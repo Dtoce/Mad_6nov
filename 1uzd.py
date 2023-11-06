95668be51bc095bbe4f5cfeee9ea0132064daf65
Kristaps Solovjovs <kristapssolovjovs1@gmail.com>
2023-11-06 09:27:50 +0200

    new

---
+++ 1uzd.py
@@ -0,0 +1,13 @@
+"""
+Ja vēlaties iegūt nejaušu skaitli no 1 līdz 10.
+
+jābūt iespējai izdrukāt datora izvēlētu skaitli, robežās no 1 līdz 10.
+"""
+import random
+
+nejauss_skaitlis=random.randint(1, 10)
+
+print(nejauss_skaitlis)
+
+# git config --global user.name "Kristaps Solovjosv"
+# git config global user.email "kristapssolovjovs1@gmail.com"
